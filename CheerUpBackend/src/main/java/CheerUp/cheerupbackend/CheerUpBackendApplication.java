e7a5f49fd95503602821a7fb782b6f403d040190
Chung yeojin <84843910+Jini-lab@users.noreply.github.com>
2023-08-15 03:03:45 +0900

    Add files via upload
    mysql 연결 전. jpa를 이용하여 data.sql에서 작성한 데이터를 포스트맨(클라이언트)에서 확인했음

---
+++ CheerUpBackend/src/main/java/CheerUp/cheerupbackend/CheerUpBackendApplication.java
@@ -0,0 +1,11 @@
+package CheerUp.cheerupbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheerUpBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CheerUpBackendApplication.class, args);
+    }
+}
